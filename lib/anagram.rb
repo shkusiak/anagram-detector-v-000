980a05aa5e05d979f07fa6833de8f6a40a42a3f8
Shea Kusiak <shkusiak@gmail.com>
2018-04-06 02:50:43 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :names
+
+  def initialize
+    @names = names
+  end
+
+  def match(anagrams_array)
+    names = names.split("")
+  end
+
+end
