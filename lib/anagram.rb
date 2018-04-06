9b060e16513d7f0ab0761eac3b1ccdb92b1723c5
Shea Kusiak <shkusiak@gmail.com>
2018-04-06 04:34:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,15 @@
 # Your code goes here!
 class Anagram
   attr_accessor :name #this is the original word we are trying to find an anagram for
-  @@positive_anagrams = [] #anagrams will be saved in this array
 
   def initialize(name)
     @name = name
   end
 
   def match(potential_anagrams)
-    @@positive_anagrams.clear
-    potential_anagrams.each do |word|
-      if (word).split("").sort == (name).split("").sort
-           @@positive_anagrams << word
-      end
+    potential_anagrams.select do |word|
+      (word).split("").sort == (name).split("").sort
     end
-    @@positive_anagrams
   end
 
 end
